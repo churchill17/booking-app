8704394d6bd9e5df49d387033c8606133cc4d94d
churchill17 <churchillamaechi17@gmailcom>
2026-05-22 11:04:45 +0100

    Remove some errors

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/booking_api": {
+        target: "https://ibooknova.com.ng",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
